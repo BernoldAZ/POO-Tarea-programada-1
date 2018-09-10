440562ad446d84a795eeaea93d7d719b43c8f2a4
Bernold <bernoldaz@gmail.com>
2018-09-10 13:46:38 -0600

    Singletone

--- POO progra/src/Index/Indexador.java
+++ POO progra/src/Index/Indexador.java
@@ -1,5 +1,41 @@
 package Index;
 
 public class Indexador {
+	private int numOfWords;
+	private String[] wordVector;
+	private double[][] tfIdfMatrix;
+	
+	private static Indexador indice;
+	
+	private Indexador() {
+		
+	}
+
+	public static Indexador getInstance() {
+		if (indice == null) {
+			indice = new Indexador();
+		}
+		return indice;
+	}
+	
+	public void useTFIDF (String[] docs) {
+		TF_IDF tfidf = new TF_IDF (docs);
+		numOfWords = tfidf.getNumOfWords();
+		wordVector = tfidf.getWordVector();
+		tfIdfMatrix = tfidf.getTF_IDFMatrix();
+		
+	}
+
+	public int getNumOfWords() {
+		return numOfWords;
+	}
+
+	public String[] getWordVector() {
+		return wordVector;
+	}
+
+	public double[][] getTfIdfMatrix() {
+		return tfIdfMatrix;
+	}
 
 }
